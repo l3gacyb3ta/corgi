66dcc76e6d2c530d3868b57522d8c1ca1e5e06a5
Gitea <gitea@fake.local>
2021-04-26 20:30:12 -0400

    Add some magic code. such as getuser, getfiles, uploadfile.

--- corgi.py
+++ corgi.py
@@ -1,19 +1,26 @@
 import requests, os
+import magic           # magic for mime types
 
-token = os.environ["TOKEN"]
-headers = {'Authorization': 'Bearer ' + token}
-baseUrl = "https://pat.doggo.ninja/v1/"
+token = os.environ["TOKEN"]                     # Get the token from an OS variable
+headers = {'Authorization': 'Bearer ' + token}  # Setup auth header
+baseUrl = "https://pat.doggo.ninja/v1/"         # Convience 
+mime = magic.Magic(mime=True)                   # setup mime getter
 
 def getuser():
+    """Gets the username."""
     r = requests.get(baseUrl + 'me', headers=headers)
     return r.json()['name']
 
 def getfiles():
+    """Get the uploaded files."""
     r = requests.get(baseUrl + 'files', headers=headers)
     return r.json()
 
+def uploadfile(path):
+    """Upload the file given in the path."""
+    return path
 
 
-print(getuser())
 
-getfiles()
+# print(getuser())
+# getfiles()
