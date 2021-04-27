fcd8f745e36c185a068adae72c6438676f368961
Gitea <gitea@fake.local>
2021-04-26 20:23:14 -0400

    Commited auto-magically because l3gacy was too lazy to actually write a commit message for this.

--- corgi.py
+++ corgi.py
@@ -1,1 +1,19 @@
-import requests
+import requests, os
+
+token = os.environ["TOKEN"]
+headers = {'Authorization': 'Bearer ' + token}
+baseUrl = "https://pat.doggo.ninja/v1/"
+
+def getuser():
+    r = requests.get(baseUrl + 'me', headers=headers)
+    return r.json()['name']
+
+def getfiles():
+    r = requests.get(baseUrl + 'files', headers=headers)
+    return r.json()
+
+
+
+print(getuser())
+
+getfiles()
